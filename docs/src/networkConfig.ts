aadaa2bf86f46a781fa27ddb57b4be064d0e0735
Thoralf-M <thoralf.mue@gmail.com>
2026-03-21 11:24:00 +0100

    testnet + mainnet deployments

--- docs/src/networkConfig.ts
+++ docs/src/networkConfig.ts
@@ -10,12 +10,14 @@ export const NETWORKS = {
   testnet: {
     url: "https://api.testnet.iota.cafe",
     graphql: "https://graphql.testnet.iota.cafe",
-    packageId: "",
+    packageId:
+      "0xfc65701aeba619e4c1e33c58738874fd76330e8a50cf6a7d34626d198407c653",
   },
   mainnet: {
     url: "https://api.mainnet.iota.cafe",
     graphql: "https://graphql.mainnet.iota.cafe",
-    packageId: "",
+    packageId:
+      "0x82db91a26597e11d40afcbee6e71d2d7816de6513fe3a95d9e56bea0aca59f04",
   },
   localnet: {
     url: "http://127.0.0.1:9000",
